34f15d542b55bd265e6ee50f95e88e948f2af57f
vsmachine <atnite15@gmail.com>
2026-05-18 15:26:06 -0500

    Add files via upload

---
+++ learning c syntax.c
@@ -0,0 +1,54 @@
+// Basic Synatx with Curly Brackets
+
+#include <stdio.h> // For input and output operations
+
+int main() {
+    {
+        printf("January, ");
+    }
+    {
+        printf("Febuary, ");
+    }
+    {
+        printf("March, ");
+    }
+    {
+        printf("April, ");
+    }
+    return 0;
+}
+
+// Main function where the program execution begins
+int main() {
+    {
+        // line that prints user details
+        printf("User Details:");
+    }
+    {
+        // line that displays users name
+        printf("Name: Adam");
+    }
+    {
+        // line that prints users age
+        printf("Age: 21");
+    }
+    
+    return 0; // end of program
+}
+
+// Code to print users name, score, and placement
+
+#include <stdio.h> // Include standard input/output header
+
+int main() {
+    // print users names
+    printf("My name is Mike. ");
+    
+    // print users score with percentage %%
+    printf("I scored a 95.5%, ");
+    
+    // print placement
+    printf("Which was the second score. ");
+    
+    return 0;
+}
